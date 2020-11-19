854474a8113dd66ad60adf3201aaa912d2545d32
mricijas <mricijas@foi.hr>
2020-11-19 19:12:30 +0100

    Izrada dizajna fragmenta za prikaz detalja ponude i popravak dizajna fragmenta za dodavanje nove ponude

--- app/src/main/java/com/example/digitalnaribarnica/Fragments/EditProfileFragment.java
+++ app/src/main/java/com/example/digitalnaribarnica/Fragments/EditProfileFragment.java
@@ -1,23 +1,44 @@
 package com.example.digitalnaribarnica.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.digitalnaribarnica.R;
+import com.example.digitalnaribarnica.databinding.FragmentEditProfileBinding;
+import com.example.digitalnaribarnica.databinding.FragmentOfferDetailBinding;
 
 public class EditProfileFragment extends Fragment {
+    FragmentEditProfileBinding binding;
+    private Button btnDetails;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit_profile,container,false);
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
+        btnDetails = binding.btnDetaljiPonude;
+
+        btnDetails.setOnClickListener(new View.OnClickListener() {
+            Fragment selectedFragment = null;
+
+            @Override
+            public void onClick(View v) {
+                Log.d("NOVITAG", "Trebalo bi pokrenuti");
+                selectedFragment = new OfferDetailFragment();
+                getFragmentManager().beginTransaction().replace(R.id.fragment_containter,
+                        selectedFragment).commit();
+            }
+        });
+
+        return  view;
     }
-
-
 }
