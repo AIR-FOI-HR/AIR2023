7f3b3374e64320061324267c659b41919f818f42
lrozic <lukarozic007@gmail.com>
2020-11-22 13:29:06 +0100

    Implementacija klase OffersData koja sadrži tipove podataka za RecyclerView.

---
+++ app/src/main/java/com/example/digitalnaribarnica/recycleviewer/OffersData.java
@@ -0,0 +1,86 @@
+package com.example.digitalnaribarnica.recycleviewer;
+
+import androidx.annotation.NonNull;
+
+public class OffersData {
+    private String name;
+    private String description;
+    private String imageurl;
+    private String price;
+    private String fishClass;
+    private String imageurltrophey;
+
+    public OffersData(String name, String description, String imageurl, String price, String fishClass, String imageurltrophey) {
+        this.name = name;
+        this.description = description;
+        this.imageurl = imageurl;
+        this.price = price;
+        this.fishClass = fishClass;
+        this.imageurltrophey = imageurltrophey;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getFishClass() {
+        return fishClass;
+    }
+
+    public void setFishClass(String fishClass) {
+        this.fishClass = fishClass;
+    }
+
+    public String getImageurltrophey() {
+        return imageurltrophey;
+    }
+
+    public void setImageurltrophey(String imageurltrophey) {
+        this.imageurltrophey = imageurltrophey;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public OffersData(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public OffersData(String name, String description, String imageurl) {
+        this.name = name;
+        this.description = description;
+        this.imageurl = imageurl;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
