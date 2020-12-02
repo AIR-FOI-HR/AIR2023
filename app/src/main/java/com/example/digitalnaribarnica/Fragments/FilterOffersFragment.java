58b7923c6d27ee902b2130e6afdbd9b954b1c046
lrozic <lukarozic007@gmail.com>
2020-12-02 21:44:55 +0100

    Implementacija autocompletea u fragment filter te izrada prilagođenog layouta za autocomplete listu riječi.

--- app/src/main/java/com/example/digitalnaribarnica/Fragments/FilterOffersFragment.java
+++ app/src/main/java/com/example/digitalnaribarnica/Fragments/FilterOffersFragment.java
@@ -5,21 +5,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.example.database.Fish;
+import com.example.digitalnaribarnica.Repository;
+import  com.example.digitalnaribarnica.databinding.FilterOffersBinding;
+
 import com.example.digitalnaribarnica.R;
 
+import java.util.ArrayList;
+
 public class FilterOffersFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    FilterOffersBinding binding;
+    private AutoCompleteTextView editFishSpecies;
+
     @SuppressLint("RestrictedApi")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FilterOffersBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
         ((AppCompatActivity) getActivity()).getSupportActionBar().setShowHideAnimationEnabled(false);
-        return inflater.inflate(R.layout.filter_offers,container,false);
+
+        editFishSpecies = binding.autoCompleteFiltriranje;
+
+        Repository repository =new Repository();
+
+        repository.DohvatiRibe(fishes -> {
+            ArrayList<String> fishArrayList=new ArrayList<>();
+            for(Fish riba: fishes){
+                fishArrayList.add(riba.getName());
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.autocomplete_custom, R.id.autocomplete_text, fishArrayList);
+            adapter.notifyDataSetChanged();
+            editFishSpecies.setAdapter(adapter);
+        });
+
+        return view;
     }
 }
