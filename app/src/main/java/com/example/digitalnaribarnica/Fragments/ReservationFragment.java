024fe703f0a595d62b78bbecdf7ab35e745178e1
BozoKvesic1998 <bozo.kvesic1@gmail.com>
2020-12-02 11:50:21 +0100

    Dodan je prikaz rezervacija, potrebno je urediti xml cardview za pojedinu rezervaciju.

--- app/src/main/java/com/example/digitalnaribarnica/Fragments/ReservationFragment.java
+++ app/src/main/java/com/example/digitalnaribarnica/Fragments/ReservationFragment.java
@@ -3,7 +3,6 @@ package com.example.digitalnaribarnica.Fragments;
 import android.annotation.SuppressLint;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,7 +10,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +20,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.database.Rezervation;
+import com.example.database.Utils.DateParse;
 import com.example.digitalnaribarnica.R;
 import com.example.digitalnaribarnica.Repository;
 import com.example.digitalnaribarnica.RezervationCallback;
@@ -31,7 +30,7 @@ import com.example.digitalnaribarnica.recycleviewer.ReservationsData;
 import com.google.android.material.button.MaterialButtonToggleGroup;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
 
 public class ReservationFragment extends Fragment {
 
@@ -85,16 +84,7 @@ public class ReservationFragment extends Fragment {
         reservations.add(new ReservationsData("Rezervacija 4","Oprisavci", "https://i.pinimg.com/236x/aa/c8/97/aac897078e2f67e83c64f52d688d771a--saltwater-tank-saltwater-aquarium.jpg", "25,00 kn", "1","https://www.iconpacks.net/icons/1/free-badge-icon-1361-thumb.png"));
         reservations.add(new ReservationsData("Rezervacija 5","Sredanci", "https://www.tportal.hr/media/thumbnail/w1000/119470.jpeg", "30,00 kn", "2","https://www.iconpacks.net/icons/1/free-badge-icon-1361-thumb.png"));
         */
-        ArrayList<Rezervation> reservation=new ArrayList<>();
-        Repository repository=new Repository();
-        repository.DohvatiRezervacije(new RezervationCallback() {
-            @Override
-            public void onCallback(ArrayList<Rezervation> rezervations) {
-                for(Rezervation r: rezervations){
-                    Toast.makeText(getActivity(), r.getDate().toDate().toString(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
 
         ReservationsAdapter adapter = new ReservationsAdapter(getActivity());
         /*
@@ -144,7 +134,25 @@ public class ReservationFragment extends Fragment {
         buttonReservation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ArrayList<Rezervation> reservation=new ArrayList<>();
+                Repository repository=new Repository();
+                repository.DohvatiRezervacije(new RezervationCallback() {
+                    @Override
+                    public void onCallback(ArrayList<Rezervation> rezervations1) {
+                        reservations.clear();
+                        for(Rezervation r: rezervations1){
+                            //Toast.makeText(getActivity(), r.getDate().toDate().toString(), Toast.LENGTH_SHORT).show();
 
+                            Calendar calendar = DateParse.dateToCalendar(r.getDate().toDate());
+                            reservations.add(new ReservationsData(r.getIDponude(),calendar.getTime().toString(), "https://i.pinimg.com/originals/dd/54/b0/dd54b0fb0c8f4af950bfb3c15baeea8b.jpg", String.valueOf(r.getKolicina()), "5","https://www.iconpacks.net/icons/1/free-badge-icon-1361-thumb.png"));
+                        }
+                        adapter.setReservations(reservations);
+
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    }
+                });
+                /*
                 reservations.clear();
                 reservations.add(new ReservationsData("Rezervacija 1","Jelas", "https://i.pinimg.com/originals/dd/54/b0/dd54b0fb0c8f4af950bfb3c15baeea8b.jpg", "25,00 kn", "5","https://www.iconpacks.net/icons/1/free-badge-icon-1361-thumb.png"));
                 reservations.add(new ReservationsData("Rezervacija 2","Jaruge", "https://i.pinimg.com/originals/21/f1/d2/21f1d20bb776dd8e774d6e36c57dc123.jpg", "30,00 kn", "3","https://www.iconpacks.net/icons/1/free-badge-icon-1361-thumb.png"));
@@ -155,6 +163,7 @@ public class ReservationFragment extends Fragment {
 
                 recyclerView.setAdapter(adapter);
                 recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                 */
             }
         });
 
@@ -219,5 +228,7 @@ public class ReservationFragment extends Fragment {
 
         super.onCreateOptionsMenu(menu, inflater);
     }
+
+
 
 }
